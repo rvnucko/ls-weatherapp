859f457bcf23f1d06621c91903f325724123c45d
Robert Vnucko <rvnucko@gmail.com>
2024-05-27 10:51:05 +0200

    pondelok rano

--- WeatherForecast/forecastweb/urls.py
+++ WeatherForecast/forecastweb/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('' ,views.home ,name= 'home'),
     path('greet' ,views.greet ,name='greet'),
-    path('hello/<str:firstName>/' ,views.hello ,name='hello_name'),
+    path('hello/<str:first_name>/' ,views.hello ,name='hello_name'),
 ]
--- WeatherForecast/forecastweb/views.py
+++ WeatherForecast/forecastweb/views.py
@@ -3,31 +3,31 @@ import requests
 from django.http import HttpResponse
 
 # Create your views here.
+
 def home(request):
+    if 'city' in request.GET:
+        city = request.GET['city']
+        api_key = "b18920447a8d00cb0dacc317061f9975"
+        weather_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}"
+        forecast_url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&units=metric&appid={api_key}"
 
-    city = "Martin"
-    apiKey = "b18920447a8d00cb0dacc317061f9975"
-    weatherURL = f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={apiKey}"
-    forecastURL = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&units=metric&appid={apiKey}"
 
-    weatherResponse = requests.get(weatherURL).json()
-    forecastResponse = requests.get(forecastURL).json()
+        weather_response = requests.get(weather_url).json()
+        forecast_response = requests.get(forecast_url).json()
 
-    context = {
-
-        'city': city,
-        'temperature': weatherResponse['main']['temp'],
-        'description': weatherResponse['weather'][0]['description'],
-        'forecast': forecastResponse['list'][0:5],
-
-    }
-
-    return render(request ,'forecastweb/home.html' ,context)
+        context = {
+                'city': city,
+                'temperature': weather_response['main']['temp'],
+                'description': weather_response['weather'][0]['description'],
+                'forecast': forecast_response['list'][0:5]
+            }
+    else:
+        context = {}  
+          
+    return render(request, 'forecastweb/home.html', context)
 
 def greet(request):
+    return HttpResponse("Hello from LogiScool server!")
 
-    return HttpResponse("Hello from Logiscool Server!")
-
-def hello(request ,firstName):
-
-    return render(request ,'forecastweb/hello.html' ,{'name': firstName})
+def hello(request, first_name):
+    return render(request, 'forecastweb/hello.html', {'name': first_name})
